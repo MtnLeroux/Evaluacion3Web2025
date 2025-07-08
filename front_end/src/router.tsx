455497ba35c307a80fb424fd8aafa4bb19164d85
MtnLeroux <martinleroux2005@gmail.com>
2025-07-08 01:16:36 -0400

    actu9.5

--- front_end/src/router.tsx
+++ front_end/src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import Home from "./views/home";
+import Home from "./views/Home";
 import Arriendos from "./views/Arriendos";
 import ActCon from "./views/ActCon";
 
